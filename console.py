041820e8fcc31c9fd62c316f8ceae2b0c5d3f4b5
Kevin Koech <kevinkoech@proton.me>
2023-07-13 10:38:19 +0300

    implement non-interactive mode functionality

--- console.py
+++ console.py
@@ -8,6 +8,7 @@ point of the command interpreter
 
 # importing cmd library
 import cmd
+import sys
 
 
 class HBNBCommand(cmd.Cmd):
@@ -18,7 +19,7 @@ class HBNBCommand(cmd.Cmd):
     prompt = "(hbnb) "
     # Creates a welcome message
     # intro = "Welcome to AirBnB Console"
-    
+
     def do_quit(self, arg):
         """
         Quits the console
@@ -33,5 +34,33 @@ class HBNBCommand(cmd.Cmd):
         print()
         return True
 
+    # test for non-interactive mode
+    # def do_greet(self, line):
+    #    print(f"hello {line}")
+
+
+def run_commands(commands):
+    """
+    Read commands from a file
+    and run them in non-interactive mode
+    """
+    hbnb_command = HBNBCommand()
+    # disables need for raw user command input
+    hbnb_command.use_rawinput = False
+    hbnb_command.stdout = sys.stdout
+    hbnb_command.stdin = sys.stdin
+    # appends command elements to end of cmd queue
+    hbnb_command.cmdqueue.extend(commands)
+    hbnb_command.cmdloop()
+
+
 if __name__ == "__main__":
-    HBNBCommand().cmdloop()
+    # check of cl arguments
+    if len(sys.argv) > 1:
+        # Non-interactive mode entry
+        with open(sys.argv[1]) as file:
+            commands = file.read().splitlines()
+            run_commands(commands)
+    else:
+        # Interactive mode entry
+        HBNBCommand().cmdloop()
