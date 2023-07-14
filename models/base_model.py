13d3561fe9257bbba8acaeb3dc9c4373101b6dc4
Kevin Koech <kevinkoech@proton.me>
2023-07-14 08:19:12 +0300

    import storage from __init__ and implement new and save methods from class FileStorage

--- models/base_model.py
+++ models/base_model.py
@@ -10,6 +10,7 @@ class BaseModel.
 # importing libraries
 from datetime import datetime
 from uuid import uuid4
+from models import storage
 
 
 class BaseModel:
@@ -34,6 +35,7 @@ class BaseModel:
             self.id = str(uuid4())
             self.created_at = datetime.now()
             self.updated_at = self.created_at
+            models.storage.new(self)
 
     def __str__(self):
         """
@@ -49,6 +51,7 @@ class BaseModel:
         with the current time
         """
         self.updated_at = datetime.now()
+        models.storage.save()
 
     def to_dict(self):
         """
