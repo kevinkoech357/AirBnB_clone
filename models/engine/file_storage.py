3bf723c722de5ef2f13ea8b03472192adb69bfbd
Kevin Koech <kevinkoech@proton.me>
2023-07-14 12:40:54 +0300

    fix json.dumps() and json.loads()

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -9,7 +9,7 @@ file to instances
 
 # importing libraries and modules
 import json
-from models.base_model import BaseModel
+import os
 
 
 class FileStorage:
@@ -32,7 +32,7 @@ class FileStorage:
         Sets in __objects the obj
         with key <obj class name>.id
         """
-        key = "{} {}".format(obj.__class__.__name__, obj.id)
+        key = "{}.{}".format(obj.__class__.__name__, obj.id)
         self.__objects[key] = obj
 
     def save(self):
@@ -44,28 +44,36 @@ class FileStorage:
         for key, value in self.__objects.items():
             object_dict[key] = value.to_dict()
 
-        with open(self.__file_path, "w") as file:
-            json.dumps(object_dict, file)
+        with open(self.__file_path, "w", encoding="utf-8") as file:
+            file.write(json.dumps(object_dict))
 
     def reload(self):
         """
         Deserialize the JSON file to objects
         """
-        try:
-            with open(self.__file_path, "r") as file:
+        # importing modules
+        from models.base_model import BaseModel
+        from models.amenity import Amenity
+        from models.city import City
+        from models.place import Place
+        from models.review import Review
+        from models.state import State
+        from models.user import User
+
+
+        if os.path.isfile(self.__file_path):
+            with open(self.__file_path, "r", encoding="utf-8") as file:
                 # read file contents
                 file_content = file.read()
                 # deserialize the json string
-                object_dict = json.load(file_content)
+                object_dict = json.loads(file_content)
                 # print(object_dict)
                 self.__objects = {}
                 for key, value in object_dict.items():
                     class_name, object_dict = key.split(".")
                     # class_object = eval(class_name) - security risk
                     self.__objects[key] = eval(value["__class__"])(**value)
-        except FileNotFoundError:  # if file is not found
-            pass
 
-if __name__ == "__main__":
-    storage = FileStorage()
-    storage.reload()
+# if __name__ == "__main__":
+#    storage = FileStorage()
+#    storage.reload()
