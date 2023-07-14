b398f9d6df02953a88d3f1c8c7cbfadbc95e3a2b
Kevin Koech <kevinkoech@proton.me>
2023-07-14 07:38:08 +0300

    create a var storage, an instance of FileStorage

---
+++ models/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+# importing FileStorage module
+from models.engine.file_storage import FileStorage
+
+# creating storage, an instance of FileStorage
+storage = FileStorage
