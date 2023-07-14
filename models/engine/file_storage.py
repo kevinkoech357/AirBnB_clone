2d7847d88f31b4598a2529f49a5a4680fa0c334b
Kevin Koech <kevinkoech@proton.me>
2023-07-14 10:27:56 +0300

    try to impelement the reload function, has errors

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -7,8 +7,9 @@ file to instances
 """
 
 
-# importing libraries
+# importing libraries and modules
 import json
+from models.base_model import BaseModel
 
 
 class FileStorage:
@@ -31,7 +32,7 @@ class FileStorage:
         Sets in __objects the obj
         with key <obj class name>.id
         """
-        key = "{} {}".format(obj__class__.name, obj.id)
+        key = "{} {}".format(obj.__class__.__name__, obj.id)
         self.__objects[key] = obj
 
     def save(self):
@@ -40,12 +41,31 @@ class FileStorage:
         JSON file (path: __file_path)
         """
         object_dict = {}
-        for key, value in self.__object.items():
+        for key, value in self.__objects.items():
             object_dict[key] = value.to_dict()
-        with open(self.__file_path, 'w') as file:
+
+        with open(self.__file_path, "w") as file:
             json.dumps(object_dict, file)
 
     def reload(self):
         """
         Deserialize the JSON file to objects
         """
+        try:
+            with open(self.__file_path, "r") as file:
+                # read file contents
+                file_content = file.read()
+                # deserialize the json string
+                object_dict = json.load(file_content)
+                # print(object_dict)
+                self.__objects = {}
+                for key, value in object_dict.items():
+                    class_name, object_dict = key.split(".")
+                    # class_object = eval(class_name) - security risk
+                    self.__objects[key] = eval(value["__class__"])(**value)
+        except FileNotFoundError:  # if file is not found
+            pass
+
+if __name__ == "__main__":
+    storage = FileStorage()
+    storage.reload()
