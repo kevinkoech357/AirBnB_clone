75c838a3ff26fd0da9e18b6d238e7f08003ad00d
Kevin Koech <kevinkoech@proton.me>
2023-07-14 10:26:48 +0300

    import class FileStorage and create it's instances in a var storage then reloads

--- models/__init__.py
+++ models/__init__.py
@@ -1,8 +1,15 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
+"""
+Creates a new instance of FileStorage
+"""
+
+
 # importing FileStorage module
 from models.engine.file_storage import FileStorage
 
+
 # creating storage, an instance of FileStorage
-storage = FileStorage
+storage = FileStorage()
+storage.reload()
