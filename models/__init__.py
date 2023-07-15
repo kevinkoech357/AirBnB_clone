03152402f23ed17819b53318eb1367ccf5630807
Willmuseve <willmuseve@gmail.com>
2023-07-15 04:32:54 -0700

    conflict changes

--- models/__init__.py
+++ models/__init__.py
@@ -1,30 +1,12 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-"""Initializes a variable `storage` to create a
-unique `FileStorage` instance for the HBNB application.
-
-"""
-
-from models.engine.file_storage import FileStorage
-
-=======
-=======
->>>>>>> 50b420b8e338b8cff0796e437ed6f2930fcc7e7a
 #!/usr/bin/python3
 
 """
 Creates a new instance of FileStorage
 """
 
-
 # importing FileStorage module
 from models.engine.file_storage import FileStorage
 
-
 # creating storage, an instance of FileStorage
-<<<<<<< HEAD
->>>>>>> 2a3efbff0bc123af7195fb69d07a0f1c2c09dd65
-=======
->>>>>>> 50b420b8e338b8cff0796e437ed6f2930fcc7e7a
 storage = FileStorage()
 storage.reload()
